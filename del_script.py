ad5bc930c6b726c0c04e1533360b3fd0487662ca
hihithisisme <soong.lexuan6@gmail.com>
2021-04-30 10:10:02 +0800

    fix: del_script

--- del_script.py
+++ del_script.py
@@ -7,23 +7,26 @@ mode = 'train'
 mode_path = os.path.join(img_dir, mode)
 cities = [c for c in os.listdir(mode_path)]
 for city in cities:
-    to_be_deleted_dir = os.path.join(mode_path, city)
-    img_paths = os.listdir(to_be_deleted_dir)
+    ref_dir = os.path.join(mode_path, city)
+    img_paths = os.listdir(ref_dir)
     img_paths.sort()
+
     for img_path in img_paths:
         img_name = img_path.split('_')
         seq = int(img_name[1])
         ref_frame = int(img_name[2])
 
+        to_be_deleted_dir = os.path.join(seq_dir, mode, city)
+
         for frame in range(0, ref_frame-3):
             try:
-                path = 'ulm_{:06d}_{:06d}_leftImg8bit.png'.format(seq, frame)
+                path = '{}_{:06d}_{:06d}_leftImg8bit.png'.format(city, seq, frame)
                 os.remove(os.path.join(to_be_deleted_dir, path))
             except:
                 print(f'{path} does not exist')
         for frame in range(ref_frame+1, 30):
             try:
-                path = 'ulm_{:06d}_{:06d}_leftImg8bit.png'.format(seq, frame)
+                path = '{}_{:06d}_{:06d}_leftImg8bit.png'.format(city, seq, frame)
                 os.remove(os.path.join(to_be_deleted_dir, path))
             except:
                 print(f'{path} does not exist')
